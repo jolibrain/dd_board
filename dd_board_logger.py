a4bdb2f5d8fd1f456a305122acca53866c68b203
eLMeR-JB <ronald-jolibrain@elmer.re>
2017-09-05 18:12:18 +0200

    Update of DDBoard: new parameters for __init__, revamp of the method(s)

--- dd_board_logger.py
+++ dd_board_logger.py
@@ -1,56 +1,55 @@
 # -*- coding:utf-8 -*-
-import json, sys, os
+import json, os
+from shutil import rmtree
 from datetime import datetime
-from tensorboard_logger import configure as tsbl_configure, log_value as tsbl_log_value
+from tensorboard_logger import configure as tbl_configure, log_value as tbl_log_value
 
 class DDBoard:
-	"""Version 0.1
-	Converts a log to "TensorBoard compatible" data.
-	- log_file = log file used for the graph
-	- run_dir = directory used by tensorboard logger
-	- flush_time = interval between 2 flushes of the log file
+	"""Version 0.3
+	Converts logs to "TensorBoard compatible" data.
+	- obs = data flow that has to be analyzed
+	- json_file = log file (if any) to be analyzed
+	- tb_dir = directory used by tensorboard logger
+	- flush_time = interval between 2 flushes of tb cache
 	"""
+	
+	# Default values
+	tb_dir = "/opt/tensorboard/runs" # useful to have a default?
+	flush_time = 2 # if needed? tb apparently refreshes/flushes its cache after each event written.
 
-	# Default values, if needed
-	log_file = "mobilenet_ilsvrc_cleaned.log"
-	run_dir = "runs/run-"
-	flush_time = 3
-
-	def __init__(self, lf, rd, ft):
-		#~ print(self.log_file, self.run_dir, self.flush_time)
-		if lf != "":
-			self.log_file = lf
+	def __init__(self, base_dir, del_dir = False):
+		if base_dir != "":
+			self.tb_dir = base_dir
 		else:
-			self.log_file = DDBoard.log_file
-
-		if rd != "":
-			if os.path.isdir(rd):
-				new_rd = rd + "-" + datetime.now().strftime('%y%m%d-%H%M%S')
-				print("{0} already existing. Creating {1} instead...".format(rd, new_rd))
-				self.run_dir = new_rd
-			else:
-				self.run_dir = rd
+			self.tb_dir = DDBoard.tb_dir
+			
+		self.comp_dir = "/run-" + datetime.now().strftime('%y%m%d-%H%M%S')
+		
+		if (del_dir):
+			if (os.path.isdir(self.tb_dir)):
+				rmtree(self.tb_dir) # cleaning of the tb run directory
 		else:
-			self.run_dir = DDBoard.run_dir + datetime.now().strftime('%y%m%d-%H%M%S')
+			if (os.path.isdir(self.tb_dir)):
+				self.comp_dir = "/" + os.listdir(self.tb_dir)[0]
 
-		if ft != "":
-			self.flush_time = ft
-		else:
-			self.flush_time = DDBoard.flush_time
+		self.run_dir = self.tb_dir + self.comp_dir
 
-	def ddb_logger(self):
-		tsbl_configure(self.run_dir, flush_secs=self.flush_time)
+		#~ if ft != "": # To be deleted if tb flushtime is finally not taken in account
+			#~ self.flush_time = ft
+		#~ else:
+			#~ self.flush_time = DDBoard.flush_time
+			
+		#~ tbl_configure(self.run_dir, flush_secs=DDBoard.flush_time)
+		tbl_configure(self.run_dir)
 
-		try:
-			log_file = open(self.log_file, 'r')
-		except IOError:
-			print ("Does {0} exist?".format(self.log_file))
-			sys.exit()
-
-		print("Converting data...")
-		for line in log_file:
-			jsonLine = json.loads(line)
-			for key in jsonLine.keys():
+	def ddb_logger(self, obs):
+		for key in obs.keys():	
 				if (key != "iteration"):
-					value = jsonLine[key]
-					tsbl_log_value(key, jsonLine[key], int(jsonLine["iteration"]))
+						value = obs[key]
+						tbl_log_value(key, obs[key], int(obs["iteration"]))
+						
+	def ddb_logger_file(self,json_file):
+		json_src = open(json_file, 'r') # Should we check the existence of the JSon source?
+		for line in json_src:
+			json_line = json.loads(line)
+			self.ddb_logger(json_line)
